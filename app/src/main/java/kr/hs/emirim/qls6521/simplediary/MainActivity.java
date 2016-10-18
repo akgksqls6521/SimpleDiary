477b2bbb80e81d0f20b2f911f5c846f50ea54764
Hanbin Ma <qls6521@naver.com>
2016-10-18 19:41:59 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/qls6521/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/qls6521/simplediary/MainActivity.java
@@ -1,13 +1,18 @@
 package kr.hs.emirim.qls6521.simplediary;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -38,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
                 String content=readDiary(fileName);
                 editDiary.setText(content);
                 butSave.setEnabled(true);
+            }
+        });
+
+        butSave.setOnClickListener(new View.OnClickListener() {//이벤트 처리자!-익명클래스를 사용해 핸들러 구현
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream out=openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents=editDiary.getText().toString();
+                    out.write(diaryContents.getBytes());//byte 자료형으로 바꿔주기
+                    out.close();
+                    Toast.makeText(getApplicationContext(),"저장이 완료되었습니다.",Toast.LENGTH_SHORT).show();//toast객체 반환받기? show!
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
